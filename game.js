bdbf6c13ba012ada25085b22bd88bbcb180b49f3
sujalgera01 <sujalgera01@gmail.com>
2020-05-14 16:45:09 +0530

    updates

--- game.js
+++ game.js
@@ -1,31 +1,43 @@
-var colors = ["rgb(255,0,0)",
-				"rgb(255,255,0)",
-				"rgb(0,255,0)",
-				"rgb(0,255,255)",
-				"rgb(0,0,255)",
-				"rgb(255,0,255)"
+var colors = ["rgb(255, 0, 0)",
+				"rgb(255, 255, 0)",
+				"rgb(0, 255, 0)",
+				"rgb(0, 255, 255)",
+				"rgb(0, 0, 255)",
+				"rgb(255, 0, 255)"
 			]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[2];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-
+var messageDisplay = document.querySelector("#message");
 
 colorDisplay.textContent = pickedColor;
 
 for(var i = 0; i<squares.length; i++){
-	squares[i].style.background = colors[i];
+	squares[i].style.backgroundColor = colors[i];
 
-	squares[i].addEventListener('click',function(){
-		var clickedColor = this.style.background;
+	squares[i].addEventListener("click",function(){
+		
+		var clickedColor = this.style.backgroundColor;
 
 		if(clickedColor === pickedColor){
-			alert("Correct!!");
+			messageDisplay.textContent = "Correct";
+			changeColors(clickedColor);
 		}
 		else{
-			alert("Wrong!!");
+			this.style.backgroundColor = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
 }
 
+function changeColors(color){
+	for(var i = 0; i<squares.length; i++){
+		squares[i].style.backgroundColor = color;
+	}
+}
 
+function pickColor(){
+	var random = Math.floor(Math.random() * colors.length); 
+	return colors[random];
+}
